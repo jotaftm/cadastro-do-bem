877409f28c940c2a8b748befb0c97e5a17e4b7e7
José Flávio <jotaftm@gmail.com>
2022-02-28 00:45:26 -0300

    feat: confirm account

--- src/pages/ConfirmUser/index.tsx
+++ src/pages/ConfirmUser/index.tsx
@@ -1,34 +1,42 @@
 import LoadingImg from "../../assets/loading.svg";
-import { Container } from "./styles";
+import { Container, FullContainer } from "./styles";
 import { useParams } from "react-router-dom";
 import { apiCadDoBem } from "../../services/api";
 import { useEffect, useState } from "react";
-
+import Logo from "../../assets/logo.svg";
 
 export const ConfirmUser = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const params: any = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const params: any = useParams();
 
-    useEffect(() => {
-        apiCadDoBem
-          .patch(`/users/${params.userId}`, {
-              confirmed: true
-          })
-          .then((res) => {
-            setIsLoading(false);
-          })
-          .catch((err) => console.error(err));
-      }, [params.userId]);
-    
+  useEffect(() => {
+    apiCadDoBem
+      .patch(`/users/${params.userId}`, {
+        confirmed: true,
+      })
+      .then((res) => {
+        setIsLoading(false);
+      })
+      .catch((err) => console.error(err));
+  }, [params.userId]);
 
-    return (
-        <Container>
-            {isLoading ? (
-                <img src={LoadingImg} alt="Loading" />
-            ) : (
-                <>
-                </>
-            )}
-        </Container>
-    );
+  return (
+    <Container>
+      {isLoading ? (
+        <img src={LoadingImg} alt="Loading" />
+      ) : (
+        <FullContainer>
+          <a
+            href="https://www.bemol.com.br/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Logo} alt="Logo Bemol" className="logoBemol" />
+          </a>
+
+          <h1>Conta confirmada com sucesso!</h1>
+        </FullContainer>
+      )}
+    </Container>
+  );
 };
--- src/pages/ConfirmUser/styles.ts
+++ src/pages/ConfirmUser/styles.ts
@@ -1,6 +1,23 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 1920px;
-  height: 1080px;
+  width: 100%;
+  height: 689px;
+  max-width: 1920px;
+  max-height: 1080px;
+  margin: auto;
+  background-color: var(--gray);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FullContainer = styled.div`
+  width: 100%;
+  height: 400px;
+  max-width: 1920px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
 `;
